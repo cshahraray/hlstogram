e65ebc38a2101ca7002e720b300f1363e3350ec3
Kamran <cshahraray@gmail.com>
2021-09-12 02:32:57 -0400

    Creates image upload and component state variables for image.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import Button from '@material-ui/core/Button';
 
 function App() {
 
@@ -14,16 +15,35 @@ function App() {
     setImageURL(URL.createObjectURL(e.target.files[0]))
   }
 
+  useEffect(() => {
+    // console.log(imageUpload)
+    // console.log(imageURL)
+      if(imageRef.current) {
+        imageRef.current.src=(imageURL)
+      }
+
+ 
+  }, [imageURL])
+
 
   return (
     <div className="App">
-     <input
+      <Button
+    component = "label"
+    type= "contained">
+    <input
       type="file"
       accept="image/*"
       id="input"
       hidden
       onChange={handleFileChange.bind(this)}
     />  
+    Upload image
+    </Button>
+
+    <img id="image-upload" ref={imageRef} alt="hello" style={{display: "none"}}></img>
+    <img id="image-upload" ref={imageRef} alt="hello"></img>
+
     </div>
   );
 }
