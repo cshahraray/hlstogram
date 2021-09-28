4a0c33950fdd1c17310f2f4b34dbcc12a400d4b9
Kamran <cshahraray@gmail.com>
2021-09-27 20:34:13 -0400

    Cleans up and adds comments.

--- src/App.js
+++ src/App.js
@@ -1,44 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
-import { cloneElement, useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Button from '@material-ui/core/Button';
-import { createImage } from './util/canvas-utils';
 import { hslToRgb, rgbToHsl } from './util/colorspace_utils';
-import { Image, Arc, Circle, Layer, Line, Stage } from 'react-konva';
+import { Image, Circle, Layer, Line, Stage } from 'react-konva';
 import { Html } from 'react-konva-utils';
 import { getShortestLongest } from './util/object-utils';
-import { deg2Rad, getCirclePoint } from './util/circle_utils';
+import { getCirclePoint } from './util/circle_utils';
 import { getOneShadeColor } from './util/shade_utils';
 
 import ImageAdjust from './imageAdjust';
-import clone from 'just-clone';
-// const getPixels = require('get-pixels')
+
 
 function App() {
   const windowWidth = window.innerWidth;
   const windowHeight= window.innerHeight;
   const circleXY = [Math.round(windowWidth/4), Math.round(windowHeight/2)];
 
+  //COMPONENT STATE VARIABLES
   const imageRef = useRef(null);
   const centerCircleRef = useRef(null);
+    //handle image upload and file reading
   const [imageUpload, setImageUpload] = useState(null);
   const [imageURL, setImageURL] = useState(null);
   const [imageData, setImageData] = useState(null);
+    //stores HLS information for visualiaztion
   const [quantizedHLSImageData, setQuantizedImageHLSData] = useState(null);
   const [imageHLSData, setImageHLSData] = useState(null);
+
+    //information about selected hues
+    //used JSON to easily add and remove values
+    //Array version b/c react can't read changes in JSON as easily
+    //(can be refactored using useReducer hook to resolve this isssue as well)
   const [selectedHues, setSelectedHues] = useState({})
-  
-  const [selectedHuesArr, setSelectedHuesArr] = useState([]);
-  const [outputImage, setOutputImage] = useState(null);
+  const [selectedHuesArr, setSelectedHuesArr] = useState([]); //
+    //store and manipulate image data for the outputed image
   const [imageCanvas, setImageCanvas] = useState(null);
 
-  // const [quantizedInfo, setQuantizeInfo] = useState(null)
+    //misc. values used in visualiaztion and for layout
   const [diameter, setDiameter] = useState(Math.round(windowHeight/10))
   const plotLength = Math.round(windowHeight/4);
 
 
-  const getPixelColor = (pixelR, pixelB, pixelG) => {
-    const color = rgbToHsl(pixelR, pixelB, pixelG)
+  //given a pixel's RGB values return a JSON with it's HSL values
+  const getPixelColor = (pixelR, pixelG, pixelB) => {
+    const color = rgbToHsl(pixelR, pixelG, pixelB)
 
     return {
       h: Math.round(color[0]),
@@ -47,6 +53,7 @@ function App() {
     }
   }
 
+  //given a JSON with a pixel's HSL values return a JSON with it's RGB values
   const convertHSL2RGB = (hslObj) => {
     const color = hslToRgb(hslObj.h, hslObj.s / 100, hslObj.l / 100)
     return {
@@ -56,14 +63,34 @@ function App() {
     }
   }
 
+  //EVENT METHODS
+  //event method for file upload
   const handleFileChange = (e) => {
     setImageUpload(e.target.files[0])
     setImageURL(URL.createObjectURL(e.target.files[0]))
-    setSelectedHues({})
+    setSelectedHues({}) //reset hue selection upon new file upload
     setSelectedHuesArr([])
   }
 
+  //handles file reading
+  //runs asynchronously after file upload as image onload function
+  const fetchImageData =  (image) => {
+    var canvas = document.createElement('canvas')
+    var ctx = canvas.getContext('2d');
+    const origWidth = image.width;
+    const origHeight = image.height;
+    // imageRef.current.width = 500;
+    // imageRef.current.height = scaleImageHeight(500, origWidth, origHeight)
+    canvas.height = image.height 
+    canvas.width = image.width
+    // console.log("image width", image.width)
+    ctx.drawImage(image, 0,0);
+    setImageData(ctx.getImageData(0, 0, canvas.width, canvas.height));
+    setImageCanvas(canvas)
+    
+  }
 
+  //event method for hue click, adds or removes hues from list of focus hues
   const handleHuesClick = (hueAngle, hues) => {
 
       if (!hues[hueAngle]) {
@@ -85,7 +112,8 @@ function App() {
       // console.log(selectedHues)
     }
   
-
+  //useEffect hooks
+  //useEffect hook to set up reading the image pixel data
   useEffect(() => {
       if(imageRef.current && imageUpload) 
       {
@@ -94,11 +122,12 @@ function App() {
       }
   }, [imageUpload])
 
+  //useEffect hook which fires after image data has been loaded and saved to
+  //component state by fetchImageData onload method to analyze the image
   useEffect(() => {
     if (imageData) 
     {  
       setImageHLSData(getImageHLSInfo(imageData))
-      setOutputImage(imageData)
     }
     }, [imageData])
 
@@ -111,153 +140,70 @@ function App() {
     }
     }, [imageHLSData])
 
-  const fetchImageData =  (image) => {
-    var canvas = document.createElement('canvas')
-    var ctx = canvas.getContext('2d');
-    const origWidth = image.width;
-    const origHeight = image.height;
-    // imageRef.current.width = 500;
-    // imageRef.current.height = scaleImageHeight(500, origWidth, origHeight)
-    canvas.height = image.height 
-    canvas.width = image.width
-    // console.log("image width", image.width)
-    ctx.drawImage(image, 0,0);
-    setImageData(ctx.getImageData(0, 0, canvas.width, canvas.height));
-    setImageCanvas(canvas)
-    
-  }
+    //redraw image when user selects hues
+    useEffect(() => {
+      if (imageRef.current && imageData) {
+        const canvas = document.createElement('canvas')
+        const context = canvas.getContext('2d')
+        canvas.height = imageRef.current.height
+        canvas.width = imageRef.current.width
+        context.putImageData(imageData, 0, 0)
+        if (selectedHuesArr.length > 0) {
+          adjustImage(canvas, context)
+        }
+        setImageCanvas(canvas)
+      }
+  
+    }, [selectedHuesArr])
 
-  // useEffect(() => {
 
-  //   const selectedHuesArr = Object.keys(selectedHues)
-  //   if (selectedHuesArr.length > 0)
-  //     {
-  //     const imageCanvas = document.createElement('canvas')
-  //     imageCanvas.width = imageRef.current.width
-  //     imageCanvas.height = imageRef.current.height
-  //     // const origImgCtx = originalImage.getContext('2d')
-  //     const imgCanvasCtx = imageCanvas.getContext('2d')
-
-      
-  //     adjustImage(imgCanvasCtx, imageData, imageCanvas.width, imageCanvas.height, selectedHuesArr)
-  //     setOutputImage(imageCanvas)
-  //   }
-  //   // if (selectedHues) {
-  //   //   setSelectedHuesArr(Object.keys(selectedHues))
-  //   //   console.log(selectedHuesArr)
-
-  //   // }
-
-  // }, [selectedHues])
-
+  //method for Canvas to generate custom colored image based on
+  //user uploaded image and user's selected focus hues
+  //given 
   const adjustImage = (canvas, ctx) => {
-    // const origData = imageData.data;
-
     let img = ctx.getImageData(0, 0, canvas.width, canvas.height)
     let data = img.data;
     let color;
     let newColor;
     if (selectedHuesArr.length > 0 ){
       for (let i=0; i < data.length; i += 4) {
-
         //data[i] = pixel red component
         //[i+1] = green component
         //[i+2] = blue
         //[i+3] = alpha channel
         color = getPixelColor(data[i], data[i+1], data[i+2])
-        // console.log(!huesArray.includes(color.h))
-        let set = new Set(selectedHuesArr)
-        // if (i < 16) {
-        //   console.log(typeof selectedHuesArr[0])
-        //     console.log(typeof color.h.toString())
-        //   console.log(set.has(color.h.toString()))
-        //   console.log(selectedHuesArr.includes(color.h.toString()))
-        // }
           if (!selectedHuesArr.includes(color.h.toString())) {
-            // console.log(color.h)
             color.s = 0
             newColor = convertHSL2RGB(color)
             data[i] = newColor.r
             data[i+1] = newColor.g
             data[i+2] = newColor.b
-            // data[i+3] = 254;
           }
       }
-      // console.log(data === imageData.data)
 
       ctx.putImageData(img, 0, 0)
     }
-
-
   }
 
+  //misc method for scaling image heights
   const scaleImageHeight = (newWidth, origWidth, origHeight) => {
     const scale = newWidth/origWidth;
     return Math.round(scale * origHeight)
   }
 
-  useEffect(() => {
-    if (imageRef.current && imageData) {
-      // console.log(imageRef.current)
-      // console.log(imageData)
-     
-      console.log(selectedHuesArr)
-      const canvas = document.createElement('canvas')
-      const context = canvas.getContext('2d')
-      canvas.height = imageRef.current.height
-      canvas.width = imageRef.current.width
-      context.putImageData(imageData, 0, 0)
-      if (selectedHuesArr.length > 0) {
-        adjustImage(canvas, context)
-      }
-      setImageCanvas(canvas)
-    }
 
-  }, [selectedHuesArr])
 
-  const renderImage = () => {
-    if (imageCanvas) {
-      // console.log(imageRef.current)
-      // console.log(imageData)
-      // const canvas = document.createElement('canvas')
-      // const context = canvas.getContext('2d')
-      // canvas.height = imageRef.current.height
-      // canvas.width = imageRef.current.width
-      // context.putImageData(imageData, 0, 0)
-      //   if (Object.keys(selectedHues).length > 0)
-      //   {
-      //     adjustImage(context, canvas.width, canvas.height, Object.keys(selectedHues))
-      //   }
-      // setImageCanvas(canvas)
-      // const imgWidth = Math.round(windowWidth / 2);
-      // const imgHeight = scaleImageHeight(imgWidth, imageRef.current.width, imageRef.current.height)
+  
 
-          return (
-        <Image
-          width={Math.round(windowWidth / 2)} 
-          height={scaleImageHeight(Math.round(windowWidth / 2), imageRef.current.width, imageRef.current.height)} 
-          x={circleXY[0] * 2} 
-          y={Math.round(circleXY[1]/2)}
-          image={imageCanvas}
-          />
-      )
 
-      // return (  
-      // <ImageAdjust 
-      //   huesArray = {Object.keys(selectedHues)}
-      //   width = {imageRef.current.width}
-      //   height = {imageRef.current.height}
-      //   origImgdata = {imageData}
-      //   xPos = {800}
-      //   yPos={400}
-      // />)
-    }
-  }
-
+  //DATA VISUALIAZTION related methods
+  //given Canvas imageData return a creates a 
+  //JSON whose keys are color hues, and values are an array whose length
+  // corresponds to the hue's frequency
   const getImageHLSInfo = (imgData) => {
     const data = imgData.data;
     let hues = { }
-    for (let i=0; i < data.length; i += 4) { //+4 = every pixel -> +8 = every other pixel
+    for (let i=0; i < data.length; i += 4) { //+4 = every pixel 
       //data[i] = pixel red component
       //[i+1] = blue component
       //[i+2] = green
@@ -272,9 +218,10 @@ function App() {
     return hues;
   }
 
-  const pseudoQuantize = (imageHSLinfo) => { // combine adjacent hues
+  //'Quantize' the HLS image by grouping information about adjacent hues
+  //so that our visualiaztion isn't crowded 
+  const pseudoQuantize = (imageHSLinfo) => {
     let quantized = {};
-    // let keys = Object.values(imageHSLinfo)
 
     for (let i = 0; i < 360; i += 5) {
       quantized[i] = []
@@ -292,9 +239,8 @@ function App() {
       
   }
 
-
-
-
+  //create graph bars around a circle to visualize hue frequency
+  //by calculating a polygon's points using radial geometry 
   const plotHue = (hueAngle, length, circleCenter, radius) => {
     // console.log(length)
 
@@ -335,36 +281,55 @@ function App() {
     }
   }
 
+  //scale plot length of hues based on the ratio of hue's frequency in the iamge to
+  //the frequency of the most frequent hue in the image
   const scaleLength = (num, max) => {
     return Math.round((num/max) * plotLength)
   }
 
+  //iteratively plot hues based on provided image HLS data
   const plotHues = (imgData) => {
     const max = getShortestLongest(imgData)[1];
   
     const keys = Object.keys(imgData);
     
     const radius = Math.round(diameter/2)
-    // console.log('max', max)
-    // console.log('keys', keys)
-    // console.log(imgData)
-    // console.log(scaleLength(imgData[keys[1]].length))
-    // console.log(scaleLength(imgData[keys[1]]))
-    // const result = []
     return(
        keys.map( key =>
         plotHue(parseInt(key), scaleLength(imgData[key].length, max), circleXY, radius)
         )
     )
-    // keys.map( key => console.log(scaleLength(imageData[key].length, max)))
+  }
 
+  const renderImage = () => {
+    if (imageCanvas) {
 
+          return (
+        <Image
+          width={Math.round(windowWidth / 2)} 
+          height={scaleImageHeight(Math.round(windowWidth / 2), imageRef.current.width, imageRef.current.height)} 
+          x={circleXY[0] * 2} 
+          y={Math.round(circleXY[1]/2)}
+          image={imageCanvas}
+          />
+      )
+
+      // return (  
+      // <ImageAdjust 
+      //   huesArray = {Object.keys(selectedHues)}
+      //   width = {imageRef.current.width}
+      //   height = {imageRef.current.height}
+      //   origImgdata = {imageData}
+      //   xPos = {800}
+      //   yPos={400}
+      // />)
+    }
   }
 
 
 
 
-
+  //return statement
   return (
     // <div className="App">
     <>
@@ -409,13 +374,6 @@ function App() {
         stroke={'black'}
       />
 
-      {/* {imageData &&  <ImageAdjust
-          width={Math.round(windowWidth / 2)} 
-          height={scaleImageHeight(Math.round(windowWidth / 2), imageRef.current.width, imageRef.current.height)} 
-          x={circleXY[0] * 2} 
-          y={Math.round(circleXY[1]/2)}
-          imageData={imageData}
-          />} */}
     {imageData && renderImage()}
 
     
